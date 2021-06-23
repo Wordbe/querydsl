ec6b5a354ad3549e83dcc07aa5fffe255fcfb7e3
Seongho Jin <seonghojin3gmail.com>
2021-06-23 23:42:52 +0900

    init: Querydsl

---
+++ src/test/java/co/wordbe/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package co.wordbe.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
