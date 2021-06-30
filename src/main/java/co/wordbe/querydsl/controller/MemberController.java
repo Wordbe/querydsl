6f52c3322d64273c3e8eba7c25a2300b977d19b2
Seongho Jin <seonghojin3gmail.com>
2021-07-01 00:01:45 +0900

    feat: profile local, test 분리, Querydsl Controller 테스트

---
+++ src/main/java/co/wordbe/querydsl/controller/MemberController.java
@@ -0,0 +1,21 @@
+package co.wordbe.querydsl.controller;
+
+import co.wordbe.querydsl.dto.MemberSearchCondition;
+import co.wordbe.querydsl.dto.MemberTeamDto;
+import co.wordbe.querydsl.repository.MemberJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class MemberController {
+    private final MemberJpaRepository memberJpaRepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
+        return memberJpaRepository.search(condition);
+    }
+}
