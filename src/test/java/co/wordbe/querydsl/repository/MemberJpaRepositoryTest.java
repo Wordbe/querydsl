432012c64ca6aa5c6e754af21a20d8b0a560250f
Seongho Jin <seonghojin3gmail.com>
2021-06-30 23:40:50 +0900

    feat: Querydsl 동적쿼리 builder

--- src/test/java/co/wordbe/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/co/wordbe/querydsl/repository/MemberJpaRepositoryTest.java
@@ -1,6 +1,11 @@
 package co.wordbe.querydsl.repository;
 
+import co.wordbe.querydsl.dto.MemberSearchCondition;
+import co.wordbe.querydsl.dto.MemberTeamDto;
 import co.wordbe.querydsl.entity.Member;
+import co.wordbe.querydsl.entity.Team;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +24,25 @@ class MemberJpaRepositoryTest {
     EntityManager em;
 
     @Autowired MemberJpaRepository memberJpaRepository;
+
+    @BeforeEach
+    public void before() {
+        //given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
 
     @Test
     public void basicTest() {
@@ -48,5 +72,18 @@ class MemberJpaRepositoryTest {
 
         List<Member> result2 = memberJpaRepository.findByUsername_Querydsl("member1");
         assertThat(result2).containsExactly(member1);
+    }
+
+    @Test
+    public void searchTest() {
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+
+        assertThat(result).extracting("username")
+                .containsExactly("member4");
     }
 }
